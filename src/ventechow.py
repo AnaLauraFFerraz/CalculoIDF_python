15841791c10a7211e0b2b1f29df6524150b37d40
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-07-26 16:09:50 -0300

    refactor code for better readability

--- src/ventechow.py
+++ src/ventechow.py
@@ -3,54 +3,55 @@ import numpy as np
 from scipy.optimize import minimize
 import pprint
 
+# Constants
+INITIAL_GUESS = [500, 0.1, 10, 0.7]
+OPTIMIZATION_BOUNDS = [(100, 2000), (0, 3), (0, 100), (0, 10)]
 
-def ventechow_calculations(k_coefficient_data, coefficients, params, time_interval, dist_r2):
-    """Calculates the initial rainfall intensity values for different return periods and time intervals."""
 
-    ventechow = pd.DataFrame()
-    ventechow["Tr_anos"] = [2, 5, 10, 20, 30, 50, 75, 100]
+def rain_intensity_calculations(k_coefficient_data, coefficients, params, time_interval, dist_r2):
+    """Calculates the initial rainfall intensity values for
+    different return periods and time intervals."""
+    idf_data = pd.DataFrame()
+    idf_data["Tr_years"] = [2, 5, 10, 20, 30, 50, 75, 100]
 
     if dist_r2["max_dist"] == "r2_log_normal" or dist_r2["max_dist"] == "r2_log_pearson":
-        ventechow["1day"] = params["meanw"] + \
+        idf_data["1day"] = params["meanw"] + \
             k_coefficient_data["k"] * params["stdw"]
     else:
-        ventechow["1day"] = params["mean"] + \
+        idf_data["1day"] = params["mean"] + \
             k_coefficient_data["k"] * params["std_dev"]
 
-    ventechow["24h"] = ventechow["1day"] * coefficients["24h"]
+    idf_data["24h"] = idf_data["1day"] * coefficients["24h"]
 
     for interval_name in time_interval.keys():
         if interval_name != "24h":
-            ventechow[interval_name] = (
-                ventechow["1day"] * coefficients[interval_name]) / time_interval[interval_name]
+            idf_data[interval_name] = (
+                idf_data["1day"] * coefficients[interval_name]) / time_interval[interval_name]
+    return idf_data
 
-    return ventechow
 
-
-def transform_dataframe(ventechow, coefficients, time_interval):
+def transform_dataframe(idf_data, coefficients, time_interval):
     """Transforms the original DataFrame to facilitate the calculation of the relative error."""
-
     rows = [
-        {"Tr (anos)": tr, "td (min)": interval_value * 60,
+        {"Tr (years)": tr, "td (min)": interval_value * 60,
          "i_real": (i_24h * coefficients[td]) / interval_value}
-        for tr in ventechow["Tr_anos"]
+        for tr in idf_data["Tr_years"]
         for td, interval_value in time_interval.items()
-        for i_24h in ventechow.loc[ventechow["Tr_anos"] == tr, "24h"]
+        for i_24h in idf_data.loc[idf_data["Tr_years"] == tr, "24h"]
     ]
     return pd.DataFrame(rows)
 
 
 def add_condition(df):
     """Adds a column to the DataFrame with the condition based on the time duration."""
-
     df["condition"] = df["td (min)"].apply(lambda x: 1 if 5 <= x <= 60 else 2)
     return df
 
 
 def apply_i_calculated(df, parameters_1, parameters_2):
-    """Applies the Ven Te Chow equation to calculate the estimated rainfall intensity (i_calculated) for each row."""
-
-    df["i_calculado"] = df.apply(
+    """Applies the Ven Te Chow equation to calculate the
+    estimated rainfall intensity (i_calculated) for each row."""
+    df["i_calculated"] = df.apply(
         lambda row: calculate_i(
             row, parameters_1) if row["condition"] == 1 else calculate_i(row, parameters_2),
         axis=1
@@ -60,88 +61,77 @@ def apply_i_calculated(df, parameters_1, parameters_2):
 
 def calculate_i(row, parameters):
     """Calculates the estimated rainfall intensity."""
-
     k, m, c, n = parameters
-    result = (k * row["Tr (anos)"] ** m) / ((c + row["td (min)"]) ** n)
+    result = (k * row["Tr (years)"] ** m) / ((c + row["td (min)"]) ** n)
     result = np.where(np.isfinite(result), result, 0)
     return result
 
 
 def optimize_parameters(df, condition):
-    initial_guess = [500, 0.1, 10, 0.7]
-    bounds = [(100, 2000), (0, 3), (0, 100), (0, 10)]
+    """Optimizes the parameters of the Ven Te Chow equation for a given condition.
+    Parameters:
+    df (pandas.DataFrame): The DataFrame containing the rainfall data.
+    condition (int): The condition to optimize for. This should be 1 for time durations between 5 and 60 minutes, and 2 for other time durations.
+    Returns: A tuple containing the optimized parameters (k, m, c, n)."""
     df_condition = df[df['condition'] == condition]
 
     result = minimize(
         objective_function,
-        initial_guess,
+        INITIAL_GUESS,
         args=(df_condition),
-        bounds=bounds,
+        bounds=OPTIMIZATION_BOUNDS,
         method="L-BFGS-B"
     )
-
     k_opt, m_opt, c_opt, n_opt = result.x
-
     return k_opt.round(4), m_opt.round(4), c_opt.round(4), n_opt.round(4)
 
 
 def objective_function(parameters, df):
     """Defines the objective function for optimization."""
-
     df_temp = df.copy()
     k, m, c, n = parameters
-    df_temp["i_calculado"] = calculate_i(df_temp, parameters)
+    df_temp["i_calculated"] = calculate_i(df_temp, parameters)
     df_temp = add_relative_error(df_temp)
-    return df_temp["erro_relativo"].sum()
+    return df_temp["relative_error"].sum()
 
 
 def recalculate_dataframe(df, parameters_1, parameters_2):
     """Recalculates the DataFrame with the optimal parameters found."""
-
     df_temp = df.copy()
-    df_temp["i_calculado"] = df_temp.apply(
+    df_temp["i_calculated"] = df_temp.apply(
         lambda row: calculate_i(
             row, parameters_1) if row["condition"] == 1 else calculate_i(row, parameters_2),
         axis=1
     )
-
     df_temp = add_relative_error(df_temp)
 
     df_interval_1 = df_temp[df_temp["condition"] == 1]
     df_interval_2 = df_temp[df_temp["condition"] == 2]
-
-    mean_relative_error_1 = df_interval_1["erro_relativo"].mean()
-    mean_relative_error_2 = df_interval_2["erro_relativo"].mean()
-
+    mean_relative_error_1 = df_interval_1["relative_error"].mean()
+    mean_relative_error_2 = df_interval_2["relative_error"].mean()
     mean_relative_errors = {
         "interval_1": mean_relative_error_1,
         "interval_2": mean_relative_error_2
     }
-
     return mean_relative_errors, df_temp
 
 
 def add_relative_error(df):
     """Adds a column to the DataFrame with the relative error."""
-
-    df["erro_relativo"] = abs(
-        (df["i_calculado"] - df["i_real"]) / df["i_real"]) * 100
+    df["relative_error"] = abs(
+        (df["i_calculated"] - df["i_real"]) / df["i_real"]) * 100
     return df
-
-
-def print_formatted_output(output):
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(output)
 
 
 def main(distribution_data, k_coefficient_data, disaggregation_data, params, time_interval, dist_r2):
     """Main function to calculate optimal parameters and recalculate the DataFrame."""
+    print(dist_r2["max_dist"])
 
-    ventechow_data = ventechow_calculations(
+    idf_data = rain_intensity_calculations(
         k_coefficient_data, disaggregation_data, params, time_interval, dist_r2)
 
     transformed_df = transform_dataframe(
-        ventechow_data, disaggregation_data, time_interval)
+        idf_data, disaggregation_data, time_interval)
 
     initial_parameters = (1000, 0.1, 10, 1)
     transformed_df = add_condition(transformed_df)
@@ -184,7 +174,8 @@ def main(distribution_data, k_coefficient_data, disaggregation_data, params, tim
     # print(f"td > 60 min: k2={k_opt2}, m2={m_opt2}, c2={c_opt2}, n2={n_opt2}")
 
     # print(f"\nErro relativo médio: {mean_relative_errors}")
-    # print_formatted_output(output)
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(output)
     # transformed_df.to_csv('transformed_df.csv', sep=',')
 
     return output
